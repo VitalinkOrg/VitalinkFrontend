f3201681b49cc787d911a7c2535ccf1f04bac12d
Robyifan孟一凡 <robyifanmengyifan@RobyifanmengyifandeMacBook-Air.local>
2023-12-31 21:43:40 -0600

    header and footer

--- components/website/footer.vue
+++ components/website/footer.vue
@@ -1,3 +1,110 @@
 <template>
-  Footer
+  <footer class="py-5 border-top border-primary border-3 shadow-lg">
+    <div class="container">
+      <div class="row row-cols-5">
+        <div class="col">
+          Vitalink
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quas voluptas
+        </div>
+        <div class="col">
+          <h4 class="text-success h6">About Us</h4>
+          <ul class="list-unstyled">
+            <li>
+              <NuxtLink href="">Mission</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Our team</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Awards</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Testimonials</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Privacy Policy</NuxtLink>
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <h4 class="text-success h6">Services</h4>
+          <ul class="list-unstyled">
+            <li>
+              <NuxtLink href="">Mission</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Our team</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Awards</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Testimonials</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Privacy Policy</NuxtLink>
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <h4 class="text-success h6">Portfolio</h4>
+          <ul class="list-unstyled">
+            <li>
+              <NuxtLink href="">Mission</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Our team</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Awards</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Testimonials</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Privacy Policy</NuxtLink>
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <h4 class="text-success h6">Contact Us</h4>
+          <ul class="list-unstyled">
+            <li>
+              <NuxtLink href="">Mission</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Our team</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Awards</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Testimonials</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink href="">Privacy Policy</NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="bg-light d-flex p-2 rounded-3 w-auto align-items-center">
+            Recibe las últimas novedades
+            <form class="d-flex ms-2">
+              <input class="form-control me-2" type="email" placeholder="Escribe tu correo electrónico"
+                aria-label="Search">
+              <button class="btn btn-warning" type="submit">Suscribirse</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <hr>
+      <div class="pt-4">
+        Copyright @ 2023 VitalinkCR | All Rights Reserved | <NuxtLink class="btn-link" href="">Terms and Conditions
+        </NuxtLink> | <NuxtLink class="btn-link">
+          Privacy Policy</NuxtLink>
+      </div>
+    </div>
+  </footer>
 </template>
