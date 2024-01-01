6603f45a81c4f225cfc04e9f4fd99fc2bd6520eb
Robyifan孟一凡 <robyifanmengyifan@RobyifanmengyifandeMacBook-Air.local>
2023-12-31 22:36:49 -0600

    quienes confian en nosotros

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,183 @@
 <template>
 	<NuxtLayout name="web">
-		<header class="d-flex align-items-center">
-			<div class="container">
-				<h1 class="display-5 text-center"><strong>Bienestar a tu alcance: encuentra y programa citas médicas</strong></h1>
-				<p class="text-center text-muted col-sm-8 mx-auto">Pon tu salud en primer lugar con nuestro directorio de médicos
-					y una
-					forma
-					conveniente de agendar tus citas.
-				</p>
-				<div class="card">
-					<div class="card-body">
-						<form class="d-flex align-items-end" action="/buscar/wovnworv">
-							<div class="row row-cols-sm-3 w-100">
-								<div class="form-group">
-									<label for="procedimiento" class="form-label">Procedimiento</label>
-									<input type="text" class="form-control" id="procedimiento" placeholder="Operacion de cataratas"
-										aria-describedby="procedimiento-help">
-									<!-- <div id="procedimiento-help" class="form-text ">We'll never share your email with anyone else.</div> -->
+		<div class="bg-light">
+			<header class="d-flex align-items-center">
+				<div class="container">
+					<h1 class="display-5 text-center"><strong>Bienestar a tu alcance: encuentra y programa citas médicas</strong>
+					</h1>
+					<p class="text-center text-muted col-sm-8 mx-auto">Pon tu salud en primer lugar con nuestro directorio de
+						médicos
+						y una
+						forma
+						conveniente de agendar tus citas.
+					</p>
+					<div class="card">
+						<div class="card-body">
+							<form class="d-flex align-items-end" action="/buscar/wovnworv">
+								<div class="row row-cols-sm-3 w-100">
+									<div class="form-group">
+										<label for="procedimiento" class="form-label">Procedimiento</label>
+										<input type="text" class="form-control" id="procedimiento" placeholder="Operacion de cataratas"
+											aria-describedby="procedimiento-help">
+										<!-- <div id="procedimiento-help" class="form-text ">We'll never share your email with anyone else.</div> -->
+									</div>
+									<div class="form-group">
+										<label for="lugar" class="form-label">Lugar</label>
+										<input type="text" class="form-control" id="lugar" aria-describedby="lugar-help"
+											placeholder="Costa Rica">
+										<!-- <div id="lugar-help" class="form-text ">We'll never share your email with anyone else.</div> -->
+									</div>
+									<div class="form-group">
+										<label for="vaucher" class="form-label">Código de Vaucher</label>
+										<input type="text" class="form-control" id="vaucher" aria-describedby="vaucher-help"
+											placeholder="Código de Vaucher">
+										<!-- <div id="vaucher-help" class="form-text ">We'll never share your email with anyone else.</div> -->
+									</div>
 								</div>
-								<div class="form-group">
-									<label for="lugar" class="form-label">Lugar</label>
-									<input type="text" class="form-control" id="lugar" aria-describedby="lugar-help"
-										placeholder="Costa Rica">
-									<!-- <div id="lugar-help" class="form-text ">We'll never share your email with anyone else.</div> -->
+								<div class="form-group col-auto ms-4">
+									<button type="submit" class="btn btn-warning px-4">Buscar</button>
 								</div>
-								<div class="form-group">
-									<label for="vaucher" class="form-label">Código de Vaucher</label>
-									<input type="text" class="form-control" id="vaucher" aria-describedby="vaucher-help"
-										placeholder="Código de Vaucher">
-									<!-- <div id="vaucher-help" class="form-text ">We'll never share your email with anyone else.</div> -->
+							</form>
+						</div>
+					</div>
+					<p class="text-center pt-5">
+						<NuxtLink class="btn-link">Solicitar un Vaucher Request</NuxtLink>
+					</p>
+				</div>
+			</header>
+
+			<section class="py-5">
+				<div class="container">
+					<h2 class="display-5 text-center col-sm-8 mx-auto">Obtenga atención en tres sencillos pasos</h2>
+
+					<div class="row row-cols-sm-3 py-5">
+						<div>
+							<div class="card rounded-4 border-0 shadow text-center">
+								<div class="card-body">
+									ICON
+									<h3 class="h6">Busca un procedimiento</h3>
+									<p class="text-muted">Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar cons
+										elementum
+										tempus hac.</p>
 								</div>
 							</div>
-							<div class="form-group col-auto ms-4">
-								<button type="submit" class="btn btn-warning px-4">Buscar</button>
+						</div>
+						<div>
+							<div class="card rounded-4 border-0 shadow text-center">
+								<div class="card-body">
+									ICON
+									<h3 class="h6">Compara ofertas</h3>
+									<p class="text-muted">Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar cons
+										elementum
+										tempus hac.</p>
+								</div>
 							</div>
-						</form>
+						</div>
+						<div>
+							<div class="card rounded-4 border-0 shadow text-center">
+								<div class="card-body">
+									ICON
+									<h3 class="h6">Reserva una cita</h3>
+									<p class="text-muted">Lorem ipsum dolor sit amet consecte tur adipiscing elit semper dalar cons
+										elementum
+										tempus hac.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="text-center">
+						<NuxtLink class="btn btn-info">Empezar ahora</NuxtLink>
 					</div>
 				</div>
-				<p class="text-center pt-5">
-					<NuxtLink class="btn-link">Solicitar un Vaucher Request</NuxtLink>
-				</p>
+			</section>
+
+
+			<section class="py-5">
+				<div class="container">
+					<div class="card bg-primary-subtle rounded-5 border-0">
+						<div class="card-body ps-5">
+							<div class="d-flex align-items-center">
+								<div class="col-sm-6">
+									<h2 class="display-6"><strong>¿Listo para empezar la búsqueda?</strong></h2>
+									<p class="text-muted mb-4">Un centro médico te está esperando</p>
+									<NuxtLink class="btn btn-primary px-4">Empezar ahora</NuxtLink>
+								</div>
+								<div>
+									<img src="@/src/assets/img-busca-centro-medico.png" alt="Busca centro medico" class="img-fluid">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+
+		<section class="bg-white py-5">
+			<div class="container-fluid">
+				<div class="row align-items-center">
+					<div class="col-sm-6">
+						<img src="@/src/assets/img-busca-centro-medico.png" alt="Busca centro medico" class="img-fluid">
+					</div>
+					<div class="col">
+						<h2 class="display-6"><strong>Números que muestran nuestro éxito</strong></h2>
+						<p class="text-muted">Lorem ipsum dolor sit amet consectetur nunc nunc sit velit eget sollicitudin sit posuere
+						</p>
+
+						<div class="row row-cols-2 py-2">
+							<div>
+								<p class="h3 text-warning"><strong>200+</strong></p>
+								<h4 class="h5">Lorem ipsum dolor</h4>
+								<p class="text-muted"><small>Lorem ipsum dolor sit amet consectetur adipiscing elitolmel.</small></p>
+							</div>
+							<div>
+								<p class="h3 text-warning"><strong>97%</strong></p>
+								<h4 class="h5">Lorem ipsum dolor</h4>
+								<p class="text-muted"><small>Lorem ipsum dolor sit amet consectetur adipiscing elitolmel.</small></p>
+							</div>
+							<div>
+								<p class="h3 text-warning"><strong>34+</strong></p>
+								<h4 class="h5">Lorem ipsum dolor</h4>
+								<p class="text-muted"><small>Lorem ipsum dolor sit amet consectetur adipiscing elitolmel.</small></p>
+							</div>
+							<div>
+								<p class="h3 text-warning"><strong>100+</strong></p>
+								<h4 class="h5">Lorem ipsum dolor</h4>
+								<p class="text-muted"><small>Lorem ipsum dolor sit amet consectetur adipiscing elitolmel.</small></p>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		</header>
+		</section>
+
+		<section class="bg-light py-4">
+			<div class="container">
+				<div class="card bg-info rounded-5 border-0 my-5">
+					<div class="card-body py-3">
+						<div class="row align-items-center">
+							<div class="col-sm-6 ps-5">
+								<h2 class="h2 text-white"><strong>Quienes confían en nosotros</strong></h2>
+								<p class="text-white">Lorem ipsum dolor sit amet consectetur adipiscing elit aliquam mauris sed ma</p>
+							</div>
+							<div class="col">
+								<div class="row row-cols-3">
+									<img src="@/src/assets/img-logo-quienes-confian.svg" alt="logo">
+									<img src="@/src/assets/img-logo-quienes-confian.svg" alt="logo">
+									<img src="@/src/assets/img-logo-quienes-confian.svg" alt="logo">
+									<img src="@/src/assets/img-logo-quienes-confian.svg" alt="logo">
+									<img src="@/src/assets/img-logo-quienes-confian.svg" alt="logo">
+									<img src="@/src/assets/img-logo-quienes-confian.svg" alt="logo">
+									<img src="@/src/assets/img-logo-quienes-confian.svg" alt="logo">
+									<img src="@/src/assets/img-logo-quienes-confian.svg" alt="logo">
+									<img src="@/src/assets/img-logo-quienes-confian.svg" alt="logo">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 	</NuxtLayout>
 </template>
 
