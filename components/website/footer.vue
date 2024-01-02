a2c20e99ab9a9750721fa8976dc967f199eb455e
Robyifan孟一凡 <robyifanmengyifan@RobyifanmengyifandeMacBook-Air.local>
2024-01-01 23:45:21 -0600

    landing page without icons or backgrounds

--- components/website/footer.vue
+++ components/website/footer.vue
@@ -3,8 +3,11 @@
     <div class="container">
       <div class="row row-cols-5">
         <div class="col">
-          <img src="@/src/assets/img-vitalink-logo.svg" alt="Vitalink">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quas voluptas
+          <img src="@/src/assets/img-vitalink-logo.svg" alt="Vitalink" class="mb-2">
+          <p>
+            <small class="text-muted">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quas
+              voluptas</small>
+          </p>
         </div>
         <div class="col">
           <h4 class="text-success h6">About Us</h4>
@@ -87,10 +90,10 @@
           </ul>
         </div>
       </div>
-      <div class="row">
-        <div class="col">
-          <div class="bg-light d-flex p-2 rounded-3 w-auto align-items-center">
-            Recibe las últimas novedades
+      <div class="row my-4">
+        <div class="col-auto">
+          <div class="bg-light d-flex py-2 px-4 rounded-4 w-auto align-items-center ">
+            <strong>Recibe las últimas novedades</strong>
             <form class="d-flex ms-2">
               <input class="form-control me-2" type="email" placeholder="Escribe tu correo electrónico"
                 aria-label="Search">
@@ -100,10 +103,14 @@
         </div>
       </div>
       <hr>
-      <div class="pt-4">
-        Copyright @ 2023 VitalinkCR | All Rights Reserved | <NuxtLink class="btn-link" href="">Terms and Conditions
-        </NuxtLink> | <NuxtLink class="btn-link">
+      <div class="my-5 d-flex">
+        Copyright @ 2023 VitalinkCR | All Rights Reserved | <NuxtLink class="btn-link px-2" href="">Terms and Conditions
+        </NuxtLink> | <NuxtLink class="btn-link px-2">
           Privacy Policy</NuxtLink>
+
+        <div class="ms-auto">
+          Icon | Icon | Icon | Icon
+        </div>
       </div>
     </div>
   </footer>
