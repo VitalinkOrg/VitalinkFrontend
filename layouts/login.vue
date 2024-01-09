753a79ac6308f9580a35d24dacaa99fa83a22654
Robyifan孟一凡 <robyifanmengyifan@RobyifanmengyifandeMacBook-Air.local>
2024-01-09 01:11:43 -0600

    login and register

---
+++ layouts/login.vue
@@ -0,0 +1,57 @@
+<template>
+  <main class="d-flex" style="background-color: #F8F8F8">
+    <section class="left text-center d-sm-flex d-none">
+      <NuxtLink class="logo" href="/"><img src="@/src/assets/img-vitalink-logo.svg" alt="Vitalink Logo"></NuxtLink>
+      <img src="@/src/assets/img-login.png" alt="">
+      <p class="fs-5 w-50 fw-normal text-muted">¡Comienza tu viaje hacia un mejor cuidado de la salud hoy mismo!</p>
+    </section>
+    <section class="right">
+      <div class="card border-0 w-100" style="background-color: #F8FAFF; max-width: 30rem">
+        <div class="card-body p-5 rounded-4">
+          <slot />
+        </div>
+      </div>
+
+    </section>
+  </main>
+</template>
+<style src="@/assets/styles/vitalink.scss" />
+
+<style lang="scss" scoped>
+main {
+  height: 100vh;
+}
+
+.logo {
+  img {
+    width: 10rem;
+  }
+}
+
+.left,
+.right {
+  width: 100%;
+  height: 100%;
+}
+
+.left {
+  background-color: #EBECF7;
+  background-image: url('@/src/assets/bg-login.svg');
+  background-size: cover;
+  background-position: center center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 1rem;
+}
+
+.right {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow-y: auto;
+}
+</style>
---
+++ pages/pacientes/login.vue
@@ -0,0 +1,69 @@
+<template>
+  <NuxtLayout name="login">
+    <h1 class="fw-semibold fs-2">Ingrese a su cuenta</h1>
+    <hr>
+    <form>
+      <div class="form-group mb-4">
+        <label for="email" class="form-label text-capitalize">Correo Electrónico</label>
+        <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+          placeholder="Escribe tu correo electrónico">
+        <!-- <div id="nombreHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+      </div>
+      <div class="form-group mb-4">
+        <label for="password" class="form-label text-capitalize">Contraseña</label>
+        <input type="password" class="form-control" id="password" placeholder="Escribe tu contraseña"
+          aria-describedby="passwordHelp">
+        <!-- <div id="passwordHelp" class="form-text">Deben ser 8 caracteres como mínimo</div> -->
+
+      </div>
+      <button type="submit" class="btn btn-primary w-100 mt-4">Ingresar</button>
+    </form>
+    <hr>
+    <p class="text-center"><small class="text-muted">O Ingresa Con</small></p>
+    <div class="text-center d-flex flex-column">
+      <button class="btn btn-light border-dark-subtle mb-3">Ingresar con Facebook</button>
+      <button class="btn btn-light border-dark-subtle mb-3">Ingresar con Google</button>
+    </div>
+    <p class="text-center">
+      <span class="text-muted">No tienes Cuenta? </span>
+      <NuxtLink href="/pacientes/registro" class="btn-link text-dark fw-medium">Registrate</NuxtLink>
+    </p>
+  </NuxtLayout>
+</template>
+
+<style lang="scss" scoped>
+main {
+  height: 100vh;
+}
+
+.logo {
+  width: 35%;
+}
+
+.left,
+.right {
+  width: 100%;
+  height: 100%;
+}
+
+.left {
+  background-color: #EBECF7;
+  background-image: url('@/src/assets/bg-login.svg');
+  background-size: cover;
+  background-position: center center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 1rem;
+}
+
+.right {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow-y: auto;
+}
+</style>
