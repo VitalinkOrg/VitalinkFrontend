c6a773b6ed4c5e6078417845918a61f5a33c017e
Robyifan孟一凡 <robyifanmengyifan@RobyifanmengyifandeMacBook-Air.local>
2024-01-08 22:38:18 -0600

    wip search

--- components/website/nav.vue
+++ components/website/nav.vue
@@ -1,3 +1,4 @@
+
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom shadow">
     <div class="container-fluid">
@@ -5,7 +6,7 @@
         aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#"><img src="@/src/assets/img-vitalink-logo.svg" alt="Vitalink"></a>
+      <NuxtLink class="navbar-brand" href="/"><img src="@/src/assets/img-vitalink-logo.svg" alt="Vitalink"></NuxtLink>
       <div class="collapse navbar-collapse " id="navbarTogglerDemo03">
         <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
           <li class="nav-item">
