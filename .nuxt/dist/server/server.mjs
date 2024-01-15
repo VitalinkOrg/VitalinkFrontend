6889de704ffe5bac66b682410ab3014e053a2a77
Robyifan孟一凡 <robyifanmengyifan@RobyifanmengyifandeMacBook-Air.local>
2024-01-15 16:10:51 -0600

    scaffolding of multiple users

--- .nuxt/dist/server/server.mjs
+++ .nuxt/dist/server/server.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///Users/lisandrobigi/Desktop/pengyi-labs/VitalinkFrontend/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
+export { default } from "file:///Users/robyifanmengyifan/Documents/Codes/Vitalink/VitalinkFrontend/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
---
+++ pages/aseguradora/login.vue
@@ -0,0 +1,5 @@
+<template>
+  <NuxtLayout name="aseguradora-login">
+    Aseguradora Login
+  </NuxtLayout>
+</template>
