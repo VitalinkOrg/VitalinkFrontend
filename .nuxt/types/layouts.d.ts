987fe7bcc736c27e9c5cd30ad54a21b6ff6c16d2
Rob Moya <lastresmoyallc@gmail.com>
2024-02-05 18:50:51 -0600

    citas medicos

--- .nuxt/types/layouts.d.ts
+++ .nuxt/types/layouts.d.ts
@@ -1,5 +1,5 @@
 import { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "admin-dashboard" | "aseguradora-dashboard" | "aseguradora-login" | "clinicas-dashboard" | "pacientes-dashboard" | "pacientes-login" | "web"
+export type LayoutKey = "admin-dashboard" | "aseguradora-dashboard" | "aseguradora-login" | "clinicas-dashboard" | "medicos-dashboard" | "pacientes-dashboard" | "pacientes-login" | "web"
 declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
---
+++ composables/useMedico.ts
@@ -0,0 +1,57 @@
+
+import { type IPaciente, type ICita, ECitaEstado, type IVaucher, EVaucherEstado, type IMedico } from '@/types'
+
+export default function useMedico() {
+
+  const citas = useState<ICita[]>('citas', () => [{
+    id: '2356374',
+    medico: { id: '32637', nombre: 'Dra. Andrea Morales Solis' },
+    paciente: 'Andrea Morales Solis',
+    fecha: '10/11/2023',
+    hora: '18:00 - 18:45',
+    procedimiento: 'Operación de Cataratas',
+    vaucher: 'AG528GE6',
+    lugar: {
+      texto: 'Hospital CIMA'
+    },
+    estado: ECitaEstado.CONCRETADA
+  },
+  {
+    id: '2356374',
+    medico: { id: '32637', nombre: 'Dra. Andrea Morales Solis' },
+    paciente: 'Carlos Rodriguez',
+
+    fecha: '10/11/2023',
+    hora: '18:00 - 18:45',
+    procedimiento: 'Operación de Cataratas',
+    vaucher: 'SDF89552',
+
+    lugar: {
+      texto: 'Hospital CIMA'
+    },
+    estado: ECitaEstado.CANCELADA
+  },
+  {
+    id: '2356374',
+    medico: { id: '32637', nombre: 'Dra. Andrea Morales Solis' },
+    paciente: 'Maria Fernandez',
+
+    fecha: '10/11/2023',
+    hora: '18:00 - 18:45',
+    procedimiento: 'Operación de Cataratas',
+    vaucher: '-',
+
+    lugar: {
+      texto: 'Hospital CIMA'
+    },
+    estado: ECitaEstado.PENDIENTE
+  }
+  ])
+
+  // const getCitas = (page: number) => { console.log(page) }
+
+
+  return {
+    citas,
+  }
+}
