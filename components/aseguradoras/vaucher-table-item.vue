5de653e4974a84df6403f812dd163667c7955ceb
Rob Moya <lastresmoyallc@gmail.com>
2024-02-12 23:02:30 -0600

    aseguradoras vauchers

--- components/aseguradoras/vaucher-table-item.vue
+++ components/aseguradoras/vaucher-table-item.vue
@@ -1,13 +1,20 @@
 <script>
 export default {
-  props: ['vaucher'],
+  props: ['vaucher', 'selectable'],
   data: () => ({
-    open: false
+    open: false,
+
   })
 }
 </script>
 <template>
   <tr>
+    <td v-if="selectable">
+      <div class="form-check">
+        <input class="form-check-input border-dark" type="checkbox" value="" :id="vaucher.id">
+        <!-- <label class="form-check-label" for="flexCheckDefault"></label> -->
+      </div>
+    </td>
     <td><span class="badge text-dark border border-dark rounded-5 w-100">{{ vaucher.id }}</span></td>
     <td>{{ vaucher.asegurado.nombre }} {{ vaucher.asegurado.apellidos }}</td>
     <td>{{ vaucher.asegurado.id }}</td>
@@ -20,7 +27,82 @@ export default {
       </button>
     </td>
   </tr>
-  <tr v-if="open">
-    <td colspan="7">Hey</td>
+  <tr v-if="open" class="shadow mb-3 rounded-4">
+    <td colspan="8" class="px-4">
+      <span class="row">
+        <span class="col-3">
+          <dl class="d-flex mb-0">
+            <dt class="fw-normal me-1 text-nowrap">Dirección:</dt>
+            <dd> Calle Ficticia, 123</dd>
+          </dl>
+          <dl class="d-flex mb-0">
+            <dt class="fw-normal me-1 text-nowrap">Número de Teléfono:</dt>
+            <dd>+506 6123-4567</dd>
+          </dl>
+          <dl class="mb-0">
+            <dt class="fw-normal me-1 text-nowrap">Dirección Física:</dt>
+            <dd>Avenida de la Salud, 123. San José, San Rafael, Costa Rica, CR-1000</dd>
+          </dl>
+        </span>
+        <span class="col">
+          <dl class="d-flex mb-0">
+            <dt class="fw-normal me-1 text-nowrap">Proveedor del Servicio:</dt>
+            <dd>Hospital CIMA</dd>
+          </dl>
+          <dl class="d-flex mb-0">
+            <dt class="fw-normal me-1 text-nowrap">Costos y Facturación:</dt>
+            <dd>23.000 USD</dd>
+          </dl>
+          <dl class="d-flex mb-0">
+            <dt class="fw-normal me-1 text-nowrap">Número de teléfono:</dt>
+            <dd>+506 7123-4567</dd>
+          </dl>
+          <dl class="mb-0">
+            <dt class="fw-normal me-1 text-nowrap">Dirección Física:</dt>
+            <dd>San José, Autop. Próspero Fernández, San José, San Rafael, Costa Rica, CR-1000</dd>
+          </dl>
+        </span>
+        <span class="col">
+          <span class="form-label">Mensaje del asegurado</span>
+          <span class="d-block p-2 bg-primary rounded-4 fw-light border" style="--bs-bg-opacity: 0.05">Solicito un voucher
+            para
+            cubrir la
+            operación
+            de cataratas en ambos
+            ojos,
+            recomendada
+            por
+            mi oftalmólogo
+            para mejorar mi visión. Esta intervención es esencial para mi calidad de vida, y agradecería mucho su apoyo en
+            este asunto.</span>
+        </span>
+      </span>
+      <span class="row">
+        <span class="col">
+          <span class="d-block p-2 bg-primary rounded-4 fw-light border" style="--bs-bg-opacity: 0.05">
+            <span class="fw-normal mb-1 d-block">
+              <AtomsIconsChatIcon /> Comentarios
+            </span>
+            <span class="bg-light d-block p-2">
+              Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Nullam tristique libero id dui vulputate, non bibendum nunc auctor.
+              Vestibulum pellentesque, tortor nec eleifend aliquam, odio libero vulputate augue, in ultricies dolor nisl
+              in
+              lorem. Sed auctor nunc nec lorem venenatis, eu interdum ante blandit. Fusce eget consectetur elit. Integer
+              tincidunt tortor vel urna fringilla congue.
+            </span>
+            <span class="text-end d-block"><strong class="fw-medium">Tomás Clark</strong> <span class="text-muted">28 de
+                Septiembre</span></span>
+          </span>
+          <span class="text-end d-block pt-3 pb-2">
+            <button class="btn btn-light border-dark fw-normal me-3">Ver Seguimiento de Reclamaciones</button>
+            <button class="btn btn-info text-white fw-normal me-3">Agregar Comentarios o Notas</button>
+            <button class="btn btn-info text-white fw-normal">
+              <AtomsIconsDownloadIcon />
+            </button>
+          </span>
+        </span>
+      </span>
+    </td>
   </tr>
 </template>
--- pages/aseguradoras/vauchers.vue
+++ pages/aseguradoras/vauchers.vue
@@ -1,5 +1,97 @@
+
+<script setup>
+const { historialVauchers } = useAseguradora()
+
+</script>
+
 <template>
   <NuxtLayout name="aseguradoras-dashboard">
-    Vauchers
+    <nav style="--bs-breadcrumb-divider: '/';" aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <NuxtLink href="/aseguradoras/inicio" class="text-muted">Inicio</NuxtLink>
+        </li>
+        <li class="breadcrumb-item active fw-semibold" aria-current="page">Gestor de Váuchers</li>
+      </ol>
+    </nav>
+    <p>
+      <span class="fw-medium fs-4">Listado de Váuchers</span>
+    </p>
+
+    <div class="d-flex align-items-end justify-content-between mb-4">
+      <ul class="nav nav-underline d-flex flex-row w-100">
+        <li class="nav-item">
+          <button class="nav-link active">Todos</button>
+        </li>
+        <li class="nav-item">
+          <button class="nav-link">Utilizados</button>
+        </li>
+        <li class="nav-item">
+          <button class="nav-link">Pendientes de aprobación</button>
+        </li>
+        <li class="nav-item">
+          <button class="nav-link">Pendientes de usar</button>
+        </li>
+        <li class="nav-item">
+          <button class="nav-link">Rechazados</button>
+        </li>
+      </ul>
+    </div>
+
+    <div class="row mb-4">
+      <div class="col-auto">
+        <div class="input-group shadow-sm">
+          <span class="input-group-text bg-transparent border-end-0 rounded-start-3" id="basic-addon1">
+            <AtomsIconsSearchIcon />
+          </span>
+          <input type="text" class="form-control border-start-0 rounded-end-3" placeholder="Buscar" aria-label="Buscar"
+            aria-describedby="basic-addon1">
+        </div>
+      </div>
+      <div class="col-auto ms-auto d-flex">
+        <button class="btn btn-outline-dark text-nowrap me-2">
+          <AtomsIconsDownloadIcon /> Descargar
+        </button>
+        <div class="dropdown">
+          <button class="btn btn-outline-dark dropdown-toggle" type="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            Ordenar por
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <section>
+      <p class="mx-2 d-flex align-items-center justify-content-between">
+        <span class="fw-semibold fs-5 px-4">Historial</span>
+        <!-- <NuxtLink class="btn btn-link text-dark" href="/aseguradoas">Ver Todo [I]</NuxtLink> -->
+      </p>
+      <div class="card ">
+        <table class="table fw-light">
+          <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col" class="text-muted">Código</th>
+              <th scope="col" class="text-muted">Nombre del asegurado</th>
+              <th scope="col" class="text-muted">No de Asegurado</th>
+              <th scope="col" class="text-muted">Procedimiento</th>
+              <th scope="col" class="text-muted">Vencimiento</th>
+              <th scope="col" class="text-muted">Estado</th>
+              <th scope="col" class="text-muted"></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <AseguradorasVaucherTableItem v-for="vaucher in historialVauchers" :vaucher="vaucher" :selectable="true" />
+          </tbody>
+        </table>
+      </div>
+
+    </section>
   </NuxtLayout>
 </template>
