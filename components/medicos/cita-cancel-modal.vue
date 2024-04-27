be397a4d3726bd37e7beee272b7f720f925979a9
bigilisandro <bigilisandro@gmail.com>
2024-04-26 21:22:22 -0300

    ayuda pages + wip filters

---
+++ components/medicos/cita-cancel-modal.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { defineProps } from "vue";
+const emit = defineEmits(["close-modal"]);
+const props = defineProps(["appointment", "open"]);
+</script>
+<template>
+  <div
+    class="modal fade"
+    id="appointment.id"
+    :class="open ? 'show' : ''"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+    v-if="appointment"
+  >
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header border-0 align-items-center d-flex">
+          <img
+            src="@/src/assets/trash.svg"
+            alt="Alerta"
+            style="height: 3rem"
+          />
+          <button
+            type="button"
+            class="btn-close btn btn-light me-2"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+            @click="emit('close-modal')"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <h5 class="fw-bold">Seguro que quieres cancelar esta cita?</h5>
+          <p>
+            Se le enviará un correo electrónico automático al usuario avisándole
+            que su cita ha sido cancelada.
+          </p>
+        </div>
+        <div class="modal-footer">
+          <div class="col">
+            <button
+              type="button"
+              class="btn btn-white border w-100"
+              data-bs-dismiss="modal"
+              @click="emit('close-modal')"
+            >
+              No, volver
+            </button>
+          </div>
+          <div class="col">
+            <button
+              type="button"
+              class="btn btn-danger border w-100"
+              data-bs-dismiss="modal"
+              @click="emit('close-modal')"
+            >
+              Si, cancelar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.show {
+  display: block;
+  background-color: rgba(0, 0, 0, 0.1);
+}
+</style>
--- components/pacientes/nav.vue
+++ components/pacientes/nav.vue
@@ -1,37 +1,65 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom shadow">
+  <nav
+    class="navbar navbar-expand-lg navbar-light bg-white border-bottom shadow"
+  >
     <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
-        aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <NuxtLink class="navbar-brand" href="/"><img src="@/src/assets/img-vitalink-logo.svg" alt="Vitalink" /></NuxtLink>
+      <NuxtLink class="navbar-brand" href="/"
+        ><img src="@/src/assets/img-vitalink-logo.svg" alt="Vitalink"
+      /></NuxtLink>
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <!-- <a class="nav-link fw-light active" aria-current="page" href="#">Home</a> -->
-            <NuxtLink href="/pacientes/inicio" class="nav-link fw-light"
-              active-class="active bg-primary-subtle rounded-3  px-3">Home
+            <NuxtLink
+              href="/pacientes/inicio"
+              class="nav-link fw-light"
+              active-class="active bg-primary-subtle rounded-3  px-3"
+              >Home
             </NuxtLink>
           </li>
           <li class="nav-item">
-            <NuxtLink href="/pacientes/citas" class="nav-link fw-light"
-              active-class="active bg-primary-subtle rounded-3  px-3">Mis Citas</NuxtLink>
+            <NuxtLink
+              href="/pacientes/citas"
+              class="nav-link fw-light"
+              active-class="active bg-primary-subtle rounded-3  px-3"
+              >Mis Citas</NuxtLink
+            >
           </li>
           <li class="nav-item">
-            <NuxtLink href="/pacientes/vauchers" class="nav-link fw-light"
-              active-class="active bg-primary-subtle rounded-3  px-3">Váuchers
+            <NuxtLink
+              href="/pacientes/vauchers"
+              class="nav-link fw-light"
+              active-class="active bg-primary-subtle rounded-3  px-3"
+              >Váuchers
             </NuxtLink>
           </li>
           <li class="nav-item">
-            <NuxtLink href="/pacientes/documentos" class="nav-link fw-light"
-              active-class="active bg-primary-subtle rounded-3  px-3">Documentos
+            <NuxtLink
+              href="/pacientes/documentos"
+              class="nav-link fw-light"
+              active-class="active bg-primary-subtle rounded-3  px-3"
+              >Documentos
             </NuxtLink>
           </li>
           <li class="nav-item">
-            <NuxtLink href="/pacientes/facturas" class="nav-link fw-light"
-              active-class="active bg-primary-subtle rounded-3  px-3">
-              Facturación</NuxtLink>
+            <NuxtLink
+              href="/pacientes/facturas"
+              class="nav-link fw-light"
+              active-class="active bg-primary-subtle rounded-3  px-3"
+            >
+              Facturación</NuxtLink
+            >
           </li>
         </ul>
       </div>
@@ -41,30 +69,42 @@
             <AtomsIconsStarOutline />
           </button>
         </li>
-        <li class="nav-item"><button class="nav-link">
+        <li class="nav-item">
+          <button class="nav-link">
             <AtomsIconsNotificationsIcon />
-          </button></li>
-        <li class="nav-item dropdown">
-          <a class="nav-link" :class="open ? 'show' : ''" @click="open = !open" href="#" role="button"
-            data-bs-toggle="dropdown" aria-expanded="false" data-bs-display="static">
-            <img src="@/src/assets/img-avatar-sm.png" alt="avatar" />
-            <AtomsIconsChevronDown />
-          </a>
-          <ul class="dropdown-menu dropdown-menu-start" :class="open ? 'show' : ''" style="transform: translateX(-5rem)">
-            <li><a class="dropdown-item" href="/pacientes/cuenta">[I] Mi Cuenta</a></li>
-            <li><a class="dropdown-item" href="#">[I] Mis Reseñas</a></li>
-            <li>
-              <a class="dropdown-item" href="#">
-                <AtomsIconsNotificationsIcon /> Notificaciones
-              </a>
-            </li>
-            <li><a class="dropdown-item" href="#">[I] Ayuda y Soporte</a></li>
-            <li>
-              <button class="dropdown-item" @click="logout">
-                [I] Cerrar Sesión
-              </button>
-            </li>
-          </ul>
+          </button>
+        </li>
+        <li class="nav-item">
+          <div class="dropdown">
+            <button
+              class="nav-link"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img src="@/src/assets/img-avatar-sm.png" alt="avatar" />
+              <AtomsIconsChevronDown />
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li>
+                <a class="dropdown-item" href="/pacientes/cuenta"
+                  >[I] Mi Cuenta</a
+                >
+              </li>
+              <li><a class="dropdown-item" href="#">[I] Mis Reseñas</a></li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  <AtomsIconsNotificationsIcon /> Notificaciones
+                </a>
+              </li>
+              <li><a class="dropdown-item" href="#">[I] Ayuda y Soporte</a></li>
+              <li>
+                <button class="dropdown-item" @click="logout">
+                  [I] Cerrar Sesión
+                </button>
+              </li>
+            </ul>
+          </div>
         </li>
       </ul>
     </div>
@@ -82,9 +122,9 @@ const router = useRouter();
 const logout = () => {
   store.authenticated = false;
   store.user = null;
-  store.role = '';
+  store.role = "";
   token.value = null;
-  
+
   router.push("/pacientes/login");
 };
 </script>
