d7d9b107c0d99242010261cb5af8d40b1aae5e5c
dowardev <gonzalo@dowar.xyz>
2025-01-08 06:02:42 -0600

    Remove .nuxt directory from repository

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/lisandrobigi/Desktop/pengyi-labs/VitalinkFrontend/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "admin-dashboard" | "aseguradoras-dashboard" | "aseguradoras-login" | "clinicas-dashboard" | "medicos-dashboard-perfil" | "medicos-dashboard" | "pacientes-dashboard-perfil" | "pacientes-dashboard" | "pacientes-login" | "web"
-declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
