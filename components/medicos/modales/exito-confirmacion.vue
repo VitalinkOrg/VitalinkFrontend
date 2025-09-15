b8dd7e838e5a2523102af62f46583d38199909c2
Alejandro Sandí <alesf22@gmail.com>
2025-09-14 22:29:03 -0600

    Refine UI and styling in success confirmation modal

--- components/medicos/modales/exito-confirmacion.vue
+++ components/medicos/modales/exito-confirmacion.vue
@@ -9,10 +9,27 @@
     @close="handleCloseModal"
   >
     <div class="success-confirmation__content">
-      <h2 class="success-confirmation__content-title">Felicidades</h2>
+      <h2 class="success-confirmation__content-title">Felicitaciones!</h2>
       <p class="success-confirmation__description">
         Acabas de confirmar la cita de valoración para tu paciente.
       </p>
+
+      <div class="success-confirmation__table-wrapper">
+        <MedicosTablaDetallesCita
+          :rows="appointmentRowsWithData"
+          :hidden-title="true"
+          :aria-label="`Detalles de la cita de ${appointment.customer.name}`"
+        >
+          <template #data-estado-cita>
+            <span
+              class="status-badge"
+              :class="getStatusClass(appointment.appointment_status.code)"
+            >
+              {{ appointment.appointment_status.value1 }}
+            </span>
+          </template>
+        </MedicosTablaDetallesCita>
+      </div>
     </div>
 
     <template #footer>
@@ -20,7 +37,7 @@
         <button
           :disabled="isLoading"
           class="success-confirmation__button--outline"
-          @click="handleCloseModal"
+          @click="handleNavigateToHome"
         >
           Ir a inicio
         </button>
@@ -37,7 +54,11 @@
 </template>
 
 <script lang="ts" setup>
-import type { Appointment } from "~/types";
+import type { TablaBaseRow } from "~/components/medicos/tabla-detalles-cita.vue";
+import { useFormat } from "~/composables/useFormat";
+import type { Appointment, AppointmentStatusCode } from "~/types";
+
+const { formatDate, formatCurrency } = useFormat();
 
 interface Props {
   appointment: Appointment;
@@ -45,8 +66,69 @@ interface Props {
 
 const props = defineProps<Props>();
 
+const appointmentRowsWithData = computed((): TablaBaseRow[] => [
+  {
+    key: "paciente",
+    header: "Paciente:",
+    value: props.appointment.customer.name,
+  },
+  {
+    key: "tipo-servicio",
+    header: "Tipo de servicio:",
+    value: props.appointment.appointment_type.name,
+  },
+  {
+    key: "fecha",
+    header: "Fecha de la cita:",
+    value: props.appointment.appointment_date,
+    class: "appointment-editor__details-row--editable",
+  },
+  {
+    key: "hora",
+    header: "Hora de la cita:",
+    value: props.appointment.appointment_hour,
+    class: "appointment-editor__details-row--editable",
+    isEndRow: true,
+  },
+  {
+    key: "motivo",
+    header: "Motivo:",
+    value: props.appointment.user_description,
+  },
+  {
+    key: "procedimiento",
+    header: "Procedimiento:",
+    value: props.appointment.package?.procedure?.name,
+  },
+  {
+    key: "costo-servicio",
+    header: "Costo del servicio:",
+    value: "A confirmar en la cita",
+  },
+  {
+    key: "fecha-solicitud",
+    header: "Fecha de la solicitud:",
+    value: formatDate(props.appointment.application_date, "short"),
+  },
+  {
+    key: "tipo-reserva",
+    header: "Tipo de reserva:",
+    value: props.appointment.reservation_type.value1,
+  },
+  {
+    key: "estado-cita",
+    header: "Estado de la cita:",
+    value: props.appointment.appointment_status.code,
+  },
+]);
+
 const isModalOpen = ref<boolean>(false);
 const isLoading = ref<boolean>(false);
+
+const handleNavigateToHome = () => {
+  handleCloseModal();
+  useRouter().push("/pacientes/inicio");
+};
 
 const handleOpenModal = () => {
   isModalOpen.value = true;
@@ -54,6 +136,21 @@ const handleOpenModal = () => {
 
 const handleCloseModal = () => {
   isModalOpen.value = false;
+};
+
+const getStatusClass = (status: AppointmentStatusCode) => {
+  const statusClassMap = {
+    CANCEL_APPOINTMENT: "status-badge--cancelled",
+    PENDING_VALORATION_APPOINTMENT: "status-badge--warning",
+    PENDING_PROCEDURE: "status-badge--warning",
+    CONFIRM_PROCEDURE: "status-badge--primary",
+    CONCRETED_APPOINTMENT: "status-badge--primary",
+    VALUED_VALORATION_APPOINTMENT: "status-badge--success",
+    CONFIRM_VALIDATION_APPOINTMENT: "status-badge--success",
+    VALUATION_PENDING_VALORATION_APPOINTMENT: "status-badge--primary",
+    WAITING_PROCEDURE: "status-badge--warning",
+  };
+  return statusClassMap[status] || "";
 };
 
 defineExpose({
@@ -105,6 +202,12 @@ defineExpose({
     letter-spacing: 0;
     text-align: center;
     color: #6d758f;
+  }
+
+  &__table-wrapper {
+    padding: 20px;
+    background-color: #f8faff;
+    border-radius: 20px;
   }
 
   &__actions {
