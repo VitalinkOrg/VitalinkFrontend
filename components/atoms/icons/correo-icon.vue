7d98783e0065e541cfe344e5df353f905638f747
Alejandro Sandí <alesf22@gmail.com>
2026-05-27 22:25:55 -0600

    Add email confirmation notice to successful reservation screen

---
+++ components/atoms/icons/correo-icon.vue
@@ -0,0 +1,7 @@
+<template>
+  <Icon name="material-symbols:mail-outline" :size="size" />
+</template>
+
+<script lang="ts" setup>
+withDefaults(defineProps<{ size?: string | number }>(), { size: 24 });
+</script>
